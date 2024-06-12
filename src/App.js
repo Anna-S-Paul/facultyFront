39c4aa94e2a65d901a693a041439683bf6b82731
AnnaJ <annaspaulimp@gmail.com>
2024-06-13 01:58:32 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddFaculty from './components/AddFaculty';
 import SearchFaculty from './components/SearchFaculty';
+import ViewFaculty from './components/ViewFaculty';
 
 function App() {
   return (
     <div>
       <AddFaculty/>
       <SearchFaculty/>
+      <ViewFaculty/>
     </div>
   );
 }
---
+++ src/components/ViewFaculty.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const ViewFaculty = () => {
+  return (
+    <div>
+
+<div className="container">
+    <div className="row g-3">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">NAME</th>
+      <th scope="col">AGE</th>
+      <th scope="col">VENUE</th>
+      <th scope="col">YEAR OF JOINING</th>
+      <th scope="col">SUBJECT</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Shidha</td>
+      <td>23</td>
+      <td>FISAT</td>
+      <td>2024</td>
+      <td>Database</td>
+    </tr>
+    <tr>
+    <td>Manju</td>
+      <td>25</td>
+      <td>FISAT</td>
+      <td>2023</td>
+      <td>Java</td>
+    </tr>
+  </tbody>
+</table>
+
+        </div>
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default ViewFaculty
