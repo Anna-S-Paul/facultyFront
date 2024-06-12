734ee9e9bfa5c15b6b13da447b6a5765db84b5cf
AnnaJ <annaspaulimp@gmail.com>
2024-06-13 01:36:52 +0530

    add done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AddFaculty from './components/AddFaculty';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AddFaculty/>
     </div>
   );
 }
---
+++ src/components/AddFaculty.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const AddFaculty = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Name:</label>
+<input type="text" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Age:</label>
+<input type="text" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+<label htmlFor="" className="form-label">Venue</label>
+<textarea type="text" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Year of joining:</label>
+<input type="text" className="form-control" />
+
+                        </div>
+                        
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Subject:</label>
+<input type="text" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6"> <br />
+
+<button className="btn btn-success">Add</button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddFaculty
