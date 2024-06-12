aa98241ed6faf801589ee5180ed325499029e75b
AnnaJ <annaspaulimp@gmail.com>
2024-06-13 02:22:01 +0530

    Navset

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import AddFaculty from './components/AddFaculty';
 import SearchFaculty from './components/SearchFaculty';
 import ViewFaculty from './components/ViewFaculty';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <AddFaculty/>
-      <SearchFaculty/>
-      <ViewFaculty/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/add' element={<AddFaculty/>}/>
+      <Route path='/search' element={<SearchFaculty/>}/>
+      <Route path='/viewall' element={<ViewFaculty/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
